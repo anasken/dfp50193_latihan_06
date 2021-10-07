f80446e189e7b8ee1d6fd2c8ffed516263a3ed71
anasken <anashalim01@gmail.com>
2021-10-07 09:29:25 +0800

    tambah decrement jumlah duit

--- index.php
+++ index.php
@@ -1,31 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Kedai Baju</title>
 </head>
+
 <body>
-<h1>Kedai baju</h1>
+    <h1>Kedai baju</h1>
     <table>
         <?php
         $baju = 78;
         $duit = 250;
-        for($x = 0; $duit < 78; $x++){
+        for ($baju = 0; $duit < 78; $baju++) {
             $hasil = $duit - $baju;
-            ?>    
-        <tr>
-            <td><?php echo $duit ?></td>
-            <td>-</td>
-            <td><?php echo $baju ?></td>
-            <td>=</td>
-            <td><?php echo $hasil ?></td>
-        
-        </tr>
+            $duit -= $baju;
+        ?>
+            <tr>
+                <td><?php echo $duit ?></td>
+                <td>-</td>
+                <td><?php echo $baju ?></td>
+                <td>=</td>
+                <td><?php echo $hasil ?></td>
+            </tr>
         <?php
-    }
-    ?>
+        }
+        ?>
     </table>
 </body>
+
 </html>
